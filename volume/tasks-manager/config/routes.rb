b07c18938deab928d424d73d4989779d10a083ba
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-02 20:04:24 +1300

    Adding show tasklist

--- volume/tasks-manager/config/routes.rb
+++ volume/tasks-manager/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
 
   resources :users, only: %i[] do
-    resources :task_lists, only: %i[create destroy index update]
+    resources :task_lists
   end
 
   mount ActionCable.server => '/cable'
--- volume/tasks-manager/spec/requests/task_lists_request_spec.rb
+++ volume/tasks-manager/spec/requests/task_lists_request_spec.rb
@@ -64,6 +64,45 @@ RSpec.describe 'TaskLists', type: :request do
     end
   end
 
+  describe '#destroy' do
+    let!(:task_list) { create(:task_list, user: registred_user) }
+
+    describe 'When user is registred' do
+      describe 'And TaskList belongs to him' do
+        describe 'And it is found' do
+          before do
+            delete("/users/#{registred_user.id}/task_lists/#{task_list.id}",
+                   headers: registred_headers)
+          end
+
+          it { expect(response).to have_http_status(:no_content) }
+        end
+
+        describe 'And it is NOT found' do
+          before do
+            delete("/users/#{registred_user.id}/task_lists/-1",
+                   headers: registred_headers)
+          end
+
+          it { expect(response).to have_http_status(:not_found) }
+
+          it 'contains updated fields from params' do
+            expect(json_response_error).to eq("Couldn't find TaskList")
+          end
+        end
+      end
+
+      describe 'And TaskList belongs to another user' do
+        before do
+          delete("/users/#{registred_user2.id}/task_lists/#{task_list.id}",
+                 headers: registred_headers2)
+        end
+
+        it { expect(response).to have_http_status(:unauthorized) }
+      end
+    end
+  end
+
   describe '#edit' do
     let!(:updated_name) { 'Updated name' }
 
@@ -118,45 +157,6 @@ RSpec.describe 'TaskLists', type: :request do
     end
   end
 
-  describe '#destroy' do
-    let!(:task_list) { create(:task_list, user: registred_user) }
-
-    describe 'When user is registred' do
-      describe 'And TaskList belongs to him' do
-        describe 'And it is found' do
-          before do
-            delete("/users/#{registred_user.id}/task_lists/#{task_list.id}",
-                   headers: registred_headers)
-          end
-
-          it { expect(response).to have_http_status(:no_content) }
-        end
-
-        describe 'And it is NOT found' do
-          before do
-            delete("/users/#{registred_user.id}/task_lists/-1",
-                   headers: registred_headers)
-          end
-
-          it { expect(response).to have_http_status(:not_found) }
-
-          it 'contains updated fields from params' do
-            expect(json_response_error).to eq("Couldn't find TaskList")
-          end
-        end
-      end
-
-      describe 'And TaskList belongs to another user' do
-        before do
-          delete("/users/#{registred_user2.id}/task_lists/#{task_list.id}",
-                 headers: registred_headers2)
-        end
-
-        it { expect(response).to have_http_status(:unauthorized) }
-      end
-    end
-  end
-
   describe '#list' do
     describe 'When user is registred' do
       let!(:task_list_list) { create_list(:task_list, 10, user: registred_user) }
@@ -169,6 +169,55 @@ RSpec.describe 'TaskLists', type: :request do
 
       it { expect(response).to have_http_status(:ok) }
       it { expect(json_response_data.length).to eq(task_list_list.length) }
+    end
+  end
+
+  describe '#show' do
+    let!(:updated_name) { 'Updated name' }
+
+    let!(:update_task_list_params) do
+      {
+        name: updated_name
+      }
+    end
+
+    describe 'When user is registred' do
+      let!(:task_list) { create(:task_list, user: registred_user) }
+
+      describe 'And TaskList belongs to him' do
+        before do
+          get("/users/#{registred_user.id}/task_lists/#{task_list.id}",
+              headers: registred_headers)
+        end
+
+        it { expect(response).to have_http_status(:ok) }
+
+        it 'contains fields' do
+          expect(json_response_data['id']).to eq(task_list.id)
+          expect(json_response_data['name']).to eq(task_list[:name])
+          expect(json_response_data['description']).to eq(task_list[:description])
+          expect(json_response_data['frequence_type']).to eq(task_list[:frequence_type])
+          expect(json_response_data['user_id']).to eq(registred_user.id)
+        end
+      end
+
+      describe 'And TaskList belongs to another user' do
+        before do
+          get("/users/#{registred_user2.id}/task_lists/#{task_list.id}",
+              headers: registred_headers2)
+        end
+
+        it { expect(response).to have_http_status(:unauthorized) }
+      end
+
+      describe 'And TaskList is not found' do
+        before do
+          get("/users/#{registred_user.id}/task_lists/-1",
+              headers: registred_headers)
+        end
+
+        it { expect(response).to have_http_status(:not_found) }
+      end
     end
   end
 end
