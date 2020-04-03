bb48509ed977486309e0287a82ddec168b0fab64
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-03 19:18:29 +1300

    Refactoring task list

--- volume/tasks-manager/app/controllers/task_lists_controller.rb
+++ volume/tasks-manager/app/controllers/task_lists_controller.rb
@@ -2,8 +2,6 @@ class TaskListsController < ApplicationController
   def create
     task_list = TaskList.new(create_params)
 
-    authorize task_list, :owner?
-
     task_list.save!
 
     render_created(task_list)
@@ -45,7 +43,8 @@ end
 private
 
 def create_params
-  params.permit(:name, :description, :frequence_type, :user_id)
+  params.permit(:name, :description, :frequence_type)
+        .merge(user_id: current_user.id)
 end
 
 def update_params
--- volume/tasks-manager/config/routes.rb
+++ volume/tasks-manager/config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
 
   resources :users, only: %i[] do
-    resources :task_lists
+    resources :task_lists, only: %i[destroy show update]
   end
 
   resources :tasks, only: %i[create destroy update]
+  resources :task_lists, only: %i[create index]
 
   mount ActionCable.server => '/cable'
 end
