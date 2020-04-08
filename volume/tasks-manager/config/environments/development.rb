434534f2f8bdd18afd6de08e1768ac205adf6015
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-08 17:23:55 +1200

    adding lograge

--- volume/tasks-manager/config/environments/development.rb
+++ volume/tasks-manager/config/environments/development.rb
@@ -44,7 +44,11 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-
+  config.logger = Logger.new(STDOUT)
+  config.lograge.enabled = true
+  config.lograge.custom_options = lambda do |event|
+    event.payload
+  end
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
