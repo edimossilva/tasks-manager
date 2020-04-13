9c67e2565f765c7da1f5311062778df699ff064f
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-13 13:23:45 +1200

    improving services sync startu

--- volume/auth-provider/config/initializers/rpc_servers/rpc_servers_initializer.rb
+++ volume/auth-provider/config/initializers/rpc_servers/rpc_servers_initializer.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
   config.after_initialize do
-    if ENV['RAILS_ENV'] == 'development'
-      # wait message broker up
-      sleep 10
-      AuthProviderServer.instance.start
-    end
+    AuthProviderServer.instance.start
   end
 end
