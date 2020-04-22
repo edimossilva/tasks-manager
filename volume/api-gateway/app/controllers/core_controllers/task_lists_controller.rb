ada16f99efd59f0f76bf7cfe1251bc154fe4d242
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-22 22:58:19 +1200

    extract logic to before

--- volume/api-gateway/app/controllers/core_controllers/task_lists_controller.rb
+++ volume/api-gateway/app/controllers/core_controllers/task_lists_controller.rb
@@ -1,5 +1,5 @@
 class TaskListController < ApplicationController
-  post '/task_lists' do
+  before do
     render_unauthorized if auth_header_token.nil?
 
     auth_response_dto = AuthServiceHelper.find_user_by_token(auth_header_token)
@@ -7,21 +7,15 @@ class TaskListController < ApplicationController
     return render_response(auth_response_dto) if auth_response_dto.error?
 
     @user_id = auth_response_dto.data['userId']
+  end
 
+  post '/task_lists' do
     response_dto = CoreServiceHelper.create_task_list(create_params.to_json)
 
     render_response(response_dto)
   end
 
   get '/task_lists' do
-    render_unauthorized if auth_header_token.nil?
-
-    auth_response_dto = AuthServiceHelper.find_user_by_token(auth_header_token)
-
-    return render_response(auth_response_dto) if auth_response_dto.error?
-
-    @user_id = auth_response_dto.data['userId']
-
     response_dto = CoreServiceHelper.get_task_lists(index_params.to_json)
 
     render_response(response_dto)
