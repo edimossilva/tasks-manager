33372721206672b5bbd8bb2e652f53c9ef12caa4
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-24 17:00:45 +1200

    creating new project using ng client

--- volume/tasks-manager-client/e2e/src/app.e2e-spec.ts
+++ volume/tasks-manager-client/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Angular: Getting Started!!');
+    expect(page.getTitleText()).toEqual('tasks-manager-client app is running!');
   });
 
   afterEach(async () => {
--- volume/tasks-manager-client/src/app/app.component.spec.ts
+++ volume/tasks-manager-client/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular: Getting Started'`, () => {
+  it(`should have as title 'tasks-manager-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular: Getting Started');
+    expect(app.title).toEqual('tasks-manager-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular: Getting Started!!');
+    expect(compiled.querySelector('.content span').textContent).toContain('tasks-manager-client app is running!');
   });
 });
