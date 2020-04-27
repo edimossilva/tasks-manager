18c34759eed6e4c2f68a720ad0870a97fe97347c
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-04-27 22:32:22 +1200

    using lazy-load on taskitems

--- volume/tasks-manager-client/src/app/app.module.ts
+++ volume/tasks-manager-client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AddTaskComponent } from './tasklist/add-task/add-task.component';
 import { TaskItemShowComponent } from './task-item-show/task-item-show.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { TaskItemListComponent } from './task-item-list/task-item-list.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     TasklistShowComponent,
     AddTaskComponent,
     TaskItemShowComponent,
+    TaskItemListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ volume/tasks-manager-client/src/app/task-item-list/task-item-list.component.ts
@@ -0,0 +1,14 @@
+import { TaskItem } from './../model/task_item';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item-list',
+  templateUrl: './task-item-list.component.html',
+  styleUrls: ['./task-item-list.component.css'],
+})
+export class TaskItemListComponent implements OnInit {
+  constructor() {}
+
+  @Input() taskItems: TaskItem[];
+  ngOnInit(): void {}
+}
