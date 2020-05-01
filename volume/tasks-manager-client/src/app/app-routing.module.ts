8cb8929b736195c107e6128707bd0760ae38c8f7
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-01 13:43:03 +1200

    adding route validation

--- volume/tasks-manager-client/src/app/app-routing.module.ts
+++ volume/tasks-manager-client/src/app/app-routing.module.ts
@@ -4,12 +4,26 @@ import { LoginComponent } from './login/login.component';
 import { TasklistListComponent } from './tasklist/tasklist-list/tasklist-list.component';
 import { TasklistCreateComponent } from './tasklist/tasklist-create/tasklist-create.component';
 import { TasklistShowComponent } from './tasklist/tasklist-show/tasklist-show.component';
+import { AuthService } from './shared/services/auth/auth.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'tasklists', component: TasklistListComponent },
-  { path: 'tasklists/create', component: TasklistCreateComponent },
-  { path: 'tasklists/:id', component: TasklistShowComponent },
+  {
+    path: 'tasklists',
+    component: TasklistListComponent,
+    canActivate: [AuthService],
+  },
+  {
+    path: 'tasklists/create',
+    component: TasklistCreateComponent,
+    canActivate: [AuthService],
+  },
+  {
+    path: 'tasklists/:id',
+    component: TasklistShowComponent,
+    canActivate: [AuthService],
+  },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
