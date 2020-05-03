c9fe2e5cccaa6d266e2ff704fd27a24f8fa1ee6f
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-04 08:44:42 +1200

    refactor redis url

--- volume/tasks-manager-core/config/initializers/sidekiq.rb
+++ volume/tasks-manager-core/config/initializers/sidekiq.rb
@@ -1,10 +1,7 @@
 if ENV['RAILS_ENV'] != 'test'
   # bundle exec sidekiq
 
-  redis_host = ENV['REDIS_HOST']
-  redis_port = ENV['REDIS_PORT']
-
-  redis_url = "redis://#{redis_host}:#{redis_port}"
+  redis_url = ENV['REDISTOGO_URL']
 
   Sidekiq.configure_server do |config|
     config.redis = { url: redis_url }
