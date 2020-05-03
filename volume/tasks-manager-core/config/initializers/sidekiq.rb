72d4bb9d4f9b193744ad61b336195b6cd191abe9
EDIMO SOUSA SILVA <edimossilva@gmail.com>
2020-05-03 22:21:06 +1200

    adding reddis config to env

--- volume/tasks-manager-core/config/initializers/sidekiq.rb
+++ volume/tasks-manager-core/config/initializers/sidekiq.rb
@@ -1,6 +1,16 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/12' }
-end
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/12' }
+if ENV['RAILS_ENV'] != 'test'
+  # bundle exec sidekiq
+
+  redis_host = ENV['REDIS_HOST']
+  redis_port = ENV['REDIS_PORT']
+
+  redis_url = "redis://#{redis_host}:#{redis_port}"
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: redis_url }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: redis_url }
+  end
 end
